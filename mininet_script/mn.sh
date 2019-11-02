9b3316f4ff428593612b4db96f14608b1b6dd747
Rishabh Gupta <rishugupta515@gmail.com>
2019-11-02 18:33:57 +0530

    mn.sh added for migration

---
+++ mininet_script/mn.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ip=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $2)
+from_switch=$1
+to_switch=$2
+container_name=$3
+
+image_name=${container_name//./_}
+
+sudo docker exec -i $from_switch bash <<EOF
+echo "$ip"
+docker container stop $container_name
+docker commit $container_name $image_name
+docker save $image_name | gzip > ${image_name}.tar.gz
+rsync -r -e ssh /root/${image_name}.tar.gz root@$ip:/root/
+rm ${image_name}.tar.gz
+docker container rm ${container_name}
+exit
+EOF
+
+echo "\nContainer $1 off and $2 is now start\n"
+
+sudo docker exec -i $to_switch bash <<EOF
+gunzip -c ${image_name}.tar.gz | docker load
+docker run -it -P -d --name=${container_name} ${image_name}:latest /bin/bash
+exit
+EOF
